2e4ba365b85669502236ef6715bceded49f3aa55
lucasjosh <jl@lucasjosh.com>
2009-02-19 06:54:00 -0800

    Adding words / urls to db, using Sqlite3

--- chap4/search_engine.rb
+++ chap4/search_engine.rb
@@ -3,16 +3,32 @@ require 'nokogiri'
 require 'set'
 require 'open-uri'
 require 'uri'
+require 'sqlite3/database'
 
 module SearchEngine
 
   class Crawler
-    attr_accessor :db_name
+    attr_accessor :db_name, :db
   
-    ignorewords = %w(the of to and a in is it)
+    IGNOREWORDS = %w(the of to and a in is it)
   
     def initialize(db_name)
       @db_name = db_name
+      @db = SQLite3::Database.new(@db_name)
+    end
+    
+    def db_create
+      return if File.exists?(@db_name)
+      @db.execute("create table urllist(url)")
+      @db.execute("create table wordlist(word)")
+      @db.execute("create table wordlocation(urlid, wordid, location)")
+      @db.execute("create table link(fromid integer, toid integer)")
+      @db.execute("create table linkwords(wordid, linkid)")
+      @db.execute("create index wordidx on wordlist(word)")
+      @db.execute("create index urlidx on urllist(url)")
+      @db.execute("create index wordurlidx on wordlocation(wordid)")
+      @db.execute("create index urltoidx on link(toid)")
+      @db.execute("create index urlfromidx on link(fromid)")
     end
   
     def db_commit
@@ -20,22 +36,44 @@ module SearchEngine
     end
   
     def get_entry_id(table, field, value, create_new = true)
-      nil
+      result = @db.execute("select rowid from #{table} where #{field}='#{value}'")
+      res = result.first
+      if res.nil?
+        @db.execute("insert into #{table} (#{field}) values ('#{value}')")
+        return @db.last_insert_row_id
+      else
+        return res[0]
+      end
     end
   
     def add_to_index(url, page)
+      return if indexed?(url)
       puts "Indexing #{url}"
+      
+      text = get_text_only(page)
+      words = separate_words(text)
+      
+      urlid = get_entry_id('urllist', 'url', url)
+      
+      words.size.times do |i|
+        word = words[i]
+        next if IGNOREWORDS.include?(word)
+        
+        wordid = get_entry_id('wordlist', 'word', word)
+        @db.execute("insert into wordlocation(urlid, wordid, location) values (#{urlid}, #{wordid}, #{i})")
+      end
+      
     end
   
     def get_text_only(page)
-      nil
+     return page.content.strip
     end
   
     def separate_words(text)
-      return nil
+      return text.downcase.split(/\W+/).reject { |x| x.empty? }
     end
   
-    def is_indexed(url)
+    def indexed?(url)
       return false
     end
   
@@ -60,7 +98,7 @@ module SearchEngine
             url = URI.join(page, link.attributes['href'].to_s).to_s
             next if url =~ /'/
             url = url.split('#')[0]
-            newpages << url if url[0,4] == "http" && !is_indexed(url)
+            newpages << url if url[0,4] == "http" && !indexed?(url)
             link_text = get_text_only(link)
             add_link_ref(page, url, link_text)
            end
@@ -77,6 +115,7 @@ module SearchEngine
   end
 end
 
-c = SearchEngine::Crawler.new('')
+c = SearchEngine::Crawler.new('engine.db')
+c.db_create
 pagelist = ['http://kiwitobes.com/wiki/Perl.html']
 c.crawl(pagelist)
